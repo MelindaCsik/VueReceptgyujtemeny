fe8ee9045dfaed883d584b5fc5876103f528785b
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2024-12-10 09:32:22 +0100

    Formázás

--- receptgyujtemeny/src/components/Receptkonyv.vue
+++ receptgyujtemeny/src/components/Receptkonyv.vue
@@ -27,40 +27,67 @@
       </div>
     </nav>
 
-    <div class="mb-3">
-      <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Keress egy recepetet...">
-    </div>
+    <div class="row">
+      <div class="mb-3 col-lg-4">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Keress egy recepetet...">
+      </div>
 
-    <select class="form-select" aria-label="Default select example">
-      <option selected>Minden nehézség</option>
-      <option value="1">Könyű</option>
-      <option value="2">Közepes</option>
-      <option value="3">Nehéz</option>
-    </select>
+      <div class="col-lg-4">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Minden nehézség</option>
+          <option value="1">Könyű</option>
+          <option value="2">Közepes</option>
+          <option value="3">Nehéz</option>
+        </select>
+      </div>
 
-    <select class="form-select" aria-label="Default select example">
-      <option selected>Elkészítési idő szerint</option>
-      <option value="1">Csökenő</option>
-      <option value="2">Növekvő</option>
-    </select>
-  </header>
-  
-  <main>
-    <div class="card" style="width:400px">
-      <img class="card-img-top" src="../assets/palacsinta.jpg" alt="Card image" style="width:100%">
-      <div class="card-body">
-        <h4 class="card-title">Palacsinta</h4>
-        <p class="card-text">e text some example text. John Some examplDoe is an architect and engineer</p>
-        <a href="#" class="btn btn-primary">See Profile</a>
+      <div class="col-lg-4">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Elkészítési idő szerint</option>
+          <option value="1">Csökenő</option>
+          <option value="2">Növekvő</option>
+        </select>
       </div>
     </div>
+  </header>
 
-    <div class="card" style="width:400px">
-      <img class="card-img-top" src="../assets/palacsinta.jpg" alt="Card image" style="width:100%">
-      <div class="card-body">
-        <h4 class="card-title">Palacsinta</h4>
-        <p class="card-text">e text some example text. John Some examplDoe is an architect and engineer</p>
-        <a href="#" class="btn btn-primary">See Profile</a>
+  <main>
+
+    <div class="row">
+      <div class="card col-lg-4">
+        <img class="card-img-top" src="../assets/palacsinta.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Palacsinta</h4>
+          <p class="card-text">e text some example text. John Some examplDoe is an architect and engineer</p>
+          <a href="#" class="btn btn-primary">Részletek</a>
+        </div>
+      </div>
+
+      <div class="card col-lg-4">
+        <img class="card-img-top" src="../assets/palacsinta.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Palacsinta</h4>
+          <p class="card-text">e text some example text. John Some examplDoe is an architect and engineer</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+
+      <div class="card col-lg-4">
+        <img class="card-img-top" src="../assets/palacsinta.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Palacsinta</h4>
+          <p class="card-text">e text some example text. John Some examplDoe is an architect and engineer</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+
+      <div class="card col-lg-4">
+        <img class="card-img-top" src="../assets/palacsinta.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Palacsinta</h4>
+          <p class="card-text">e text some example text. John Some examplDoe is an architect and engineer</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
       </div>
     </div>
 
@@ -69,4 +96,16 @@
   <footer></footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar{
+  padding: 10px 50px 10px 50px;
+}
+.card {
+  width: 350px;
+  margin: 25px;
+  padding: 15px;
+}
+.card-img-top {
+ width: 100%;
+}
+</style>
